a00a814c70edf9b968f85a390d4b764051cadfbb
Егор <papakin06@mail.ru>
2026-03-09 23:42:06 +0300

    feat: change migrations; add new profile; need to fix flyway

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -110,7 +110,7 @@ CREATE TABLE mems_metrics (
     gyroscope_z DOUBLE PRECISION
 );
 
-CREATE TABLE nlb_metrics (
+CREATE TABLE nfb_metrics (
     id BIGSERIAL PRIMARY KEY,
     expedition_id BIGSERIAL,
     individual_number VARCHAR(255) NOT NULL,
@@ -151,4 +151,165 @@ CREATE TABLE productivity_metrics (
     reverse_fatigue DOUBLE PRECISION,
     relaxation DOUBLE PRECISION,
     concentration DOUBLE PRECISION
+);
+
+CREATE TABLE cardio_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    individual_number VARCHAR(255) NOT NULL,
+    expedition_id VARCHAR(255),
+    timestamp BIGINT NOT NULL,
+    session INTEGER,
+    heart_rate DOUBLE PRECISION,
+    has_artifacts INTEGER,
+    kaplan_index DOUBLE PRECISION,
+    metrics_available INTEGER,
+    motion_artifacts INTEGER,
+    skin_contact INTEGER,
+    stress_index DOUBLE PRECISION
+);
+
+CREATE TABLE eeg_artifacts_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    artifacts_channel_1 INTEGER,
+    artifacts_channel_2 INTEGER,
+    quality_channel_1 INTEGER,
+    quality_channel_2 INTEGER,
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER
+);
+
+CREATE TABLE eeg_proceed_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    channel_1 REAL,
+    channel_2 REAL,
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER
+);
+
+CREATE TABLE eeg_raw_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    channel_1 REAL,
+    channel_2 REAL,
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER
+);
+
+CREATE TABLE emotional_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER,
+    attention DOUBLE PRECISION,
+    relaxation DOUBLE PRECISION,
+    cognitive_load DOUBLE PRECISION,
+    cognitive_control DOUBLE PRECISION,
+    self_control DOUBLE PRECISION
+);
+
+CREATE TABLE mems_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER,
+    accelerometer_x DOUBLE PRECISION,
+    accelerometer_y DOUBLE PRECISION,
+    accelerometer_z DOUBLE PRECISION,
+    gyroscope_x DOUBLE PRECISION,
+    gyroscope_y DOUBLE PRECISION,
+    gyroscope_z DOUBLE PRECISION
+);
+
+CREATE TABLE nfb_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER,
+    alpha DOUBLE PRECISION,
+    beta DOUBLE PRECISION,
+    theta DOUBLE PRECISION,
+    delta DOUBLE PRECISION,
+    smr DOUBLE PRECISION
+);
+
+CREATE TABLE physiological_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER,
+    relax DOUBLE PRECISION,
+    fatigue DOUBLE PRECISION,
+    none DOUBLE PRECISION,
+    concentration DOUBLE PRECISION,
+    involvement DOUBLE PRECISION,
+    stress DOUBLE PRECISION,
+    nfb_artifacts INTEGER,
+    cardio_artifacts INTEGER
+);
+
+CREATE TABLE productivity_metrics_compressed (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    individual_number VARCHAR(255) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    session INTEGER,
+    gravity DOUBLE PRECISION,
+    productivity DOUBLE PRECISION,
+    fatigue DOUBLE PRECISION,
+    reverse_fatigue DOUBLE PRECISION,
+    relaxation DOUBLE PRECISION,
+    concentration DOUBLE PRECISION
+);
+
+CREATE TABLE physiological_baseline (
+    id BIGSERIAL PRIMARY KEY,
+    individual_number VARCHAR(255),
+    timestamp BIGINT NOT NULL,
+    expedition_id VARCHAR(255),
+    session INTEGER,
+    alpha DOUBLE PRECISION,
+    beta DOUBLE PRECISION,
+    alpha_gravity DOUBLE PRECISION,
+    beta_gravity DOUBLE PRECISION,
+    concentration DOUBLE PRECISION
+);
+
+CREATE TABLE productivity_baseline (
+    id BIGSERIAL PRIMARY KEY,
+    expedition_id VARCHAR(255),
+    individual_number VARCHAR(255),
+    timestamp BIGINT NOT NULL,
+    session INTEGER,
+    gravity DOUBLE PRECISION,
+    productivity DOUBLE PRECISION,
+    fatigue DOUBLE PRECISION,
+    reverse_fatigue DOUBLE PRECISION,
+    relaxation DOUBLE PRECISION,
+    concentration DOUBLE PRECISION
+);
+
+CREATE TABLE productivity_index (
+   id BIGSERIAL PRIMARY KEY,
+   expedition_id VARCHAR(255),
+   individual_number VARCHAR(255),
+   timestamp BIGINT NOT NULL,
+   session INTEGER,
+   relaxation VARCHAR(255),
+   stress VARCHAR(255),
+   gravity_baseline DOUBLE PRECISION,
+   productivity_baseline DOUBLE PRECISION,
+   fatigue_baseline DOUBLE PRECISION,
+   reverse_fatigue_baseline DOUBLE PRECISION,
+   relaxation_baseline DOUBLE PRECISION,
+   concentration_baseline DOUBLE PRECISION,
+   has_artifacts BOOLEAN
 );
