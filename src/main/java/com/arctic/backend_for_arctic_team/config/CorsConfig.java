ab015871601e42fe3729d106e99a4c95a86c3a43
Егор <papakin06@mail.ru>
2026-04-05 19:08:43 +0300

    change cors

--- src/main/java/com/arctic/backend_for_arctic_team/config/CorsConfig.java
+++ src/main/java/com/arctic/backend_for_arctic_team/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
