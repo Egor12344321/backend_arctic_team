7a312e3ebeffb3cdbf538b0af846a4cb2ff0379d
Егор <papakin06@mail.ru>
2026-04-05 12:18:23 +0300

    change appl.yml

--- src/main/java/com/arctic/backend_for_arctic_team/auth/service_implementation/authentication/cache/RedisCacheServiceImpl.java
+++ src/main/java/com/arctic/backend_for_arctic_team/auth/service_implementation/authentication/cache/RedisCacheServiceImpl.java
@@ -19,25 +19,25 @@ public class RedisCacheServiceImpl implements CacheService {
 
     @Override
     public void saveToCache(String individualNumber, String token) {
-        log.info("Saving refreshToken to cache started for user: {}", individualNumber);
+        log.debug("Saving refreshToken to cache started for user: {}", individualNumber);
         redisTemplate.opsForValue().set(REFRESH_REDIS_CACHE_PREFIX + individualNumber, token, Duration.ofDays(7));
 
     }
 
     @Override
     public void removeFromCache(String individualNumber) {
-        log.info("Removing refresh token from cache for user: {}", individualNumber);
+        log.debug("Removing refresh token from cache for user: {}", individualNumber);
         Boolean deleted = redisTemplate.delete(REFRESH_REDIS_CACHE_PREFIX + individualNumber);
         if (deleted){
             log.info("Cache refreshToken removed SUCCESSFULLY for user with redis-prefix: {}", REFRESH_REDIS_CACHE_PREFIX + individualNumber);
         } else {
-            log.info("Cache refreshToken removing FAILED for user with redis-prefix: {}", REFRESH_REDIS_CACHE_PREFIX + individualNumber);
+            log.error("Cache refreshToken removing FAILED for user with redis-prefix: {}", REFRESH_REDIS_CACHE_PREFIX + individualNumber);
         }
     }
 
     @Override
     public Optional<String> getFromCache(String individualNumber) {
-        log.info("Started getting refreshToken for user: {}", individualNumber);
+        log.debug("Started getting refreshToken for user: {}", individualNumber);
         return Optional.ofNullable(redisTemplate.opsForValue().get(REFRESH_REDIS_CACHE_PREFIX + individualNumber));
     }
 }
